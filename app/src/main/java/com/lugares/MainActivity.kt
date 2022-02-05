0f163e7801005a898373703653c4c938d2451ca4
Diogo15 <luisdiegore@hotmail.com>
2022-02-04 20:26:19 -0600

    click added

--- app/src/main/java/com/lugares/MainActivity.kt
+++ app/src/main/java/com/lugares/MainActivity.kt
@@ -2,6 +2,7 @@ package com.lugares
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.lugares.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.btnLogin.setOnClickListener { hacerLogin() }
+    }
+
+    fun hacerLogin(){
+        Log.d("YAHOO", "CLICKKK")
     }
 }
