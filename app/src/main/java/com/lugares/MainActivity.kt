50f1f66b2fe091eea766419df4521f6267ee8cb3
Diogo15 <luisdiegore@hotmail.com>
2022-02-04 20:18:57 -0600

    Binding added

--- app/src/main/java/com/lugares/MainActivity.kt
+++ app/src/main/java/com/lugares/MainActivity.kt
@@ -2,8 +2,12 @@ package com.lugares
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lugares.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
