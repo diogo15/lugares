339bf4efe82ed3409d3766489c3e721e4cbd4439
Diogo15 <luisdiegore@hotmail.com>
2022-02-18 18:04:18 -0600

    Firebase Implemented

--- app/src/main/java/com/lugares/MainActivity.kt
+++ app/src/main/java/com/lugares/MainActivity.kt
@@ -2,10 +2,61 @@ package com.lugares
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.lugares.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+
+        binding.btLogin.setOnClickListener { haceLogin() }
+        binding.btRegister.setOnClickListener { haceRegister() }
+
+    }
+
+    private fun haceLogin(){
+        val email = binding.etEmail.toString()
+        val clave = binding.etClave.toString()
+
+        auth.signInWithEmailAndPassword(email,clave)
+        .addOnCompleteListener(this){ task ->
+            if(task.isSuccessful){
+                Log.d("Auth", "Usuario creado")
+                val user = auth.currentUser
+                actualiza(user)
+            }
+        }
+    }
+    private fun haceRegister(){
+        val email = binding.etEmail.toString()
+        val clave = binding.etClave.toString()
+
+        auth.createUserWithEmailAndPassword(email,clave)
+            .addOnCompleteListener(this){ task ->
+                if(task.isSuccessful){
+                    Log.d("Auth", "Usuario creado")
+                    val user = auth.currentUser
+                    actualiza(user)
+                }
+            }
+    }
+
+    private fun actualiza(user: FirebaseUser? ){
+
+
     }
 }
